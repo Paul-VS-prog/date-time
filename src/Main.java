7e6b3120862ae6a6159678bbd6d9adb29beb79b2
Paul-VS-prog <74456837+Paul-VS-prog@users.noreply.github.com>
2021-11-27 21:09:09 +0300

    Hello World

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
         System.out.println();
